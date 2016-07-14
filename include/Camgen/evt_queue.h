459ea1beb87487f3a99688d7463ce8c3bfe776a8
gvdoord <g.vandenOord@gmail.com>
2016-07-15 00:37:52 +0200

    Added event queue interface

---
+++ include/Camgen/evt_queue.h
@@ -0,0 +1,100 @@
+//
+// This file is part of the CAMGEN library.
+// Copyright (C) 2013 Gijs van den Oord.
+// CAMGEN is licensed under the GNU GPL, version 2,
+// see COPYING for details.
+//
+
+/*! \file evt_queue.h
+  \brief Event interface copying input events into a queue on the heap.
+*/
+
+#ifndef CAMGEN_EVT_QUEUE_H_
+#define CAMGEN_EVT_QUEUE_H_
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Partial implementation of the event stream interface that *
+ * pushes the events to a queue in memory.                   *
+ *                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <Camgen/if_base.h>
+#include <queue>
+
+namespace Camgen
+{
+    /// Partial event interface implementation copying events to a queue in memory.
+
+    template<class model_t,std::size_t N_in,std::size_t N_out>class event_queue: public interface_base<model_t,N_in,N_out>
+    {
+        typedef interface_base<model_t,N_in,N_out> base_type;
+
+        public:
+
+            /* Public type definitions: */
+
+            typedef typename base_type::event_type event_type;
+            typedef event_type value_type;
+            typedef typename std::queue<event_type>::size_type size_type;
+
+            /// Virtual destructor.
+
+            virtual ~event_queue(){}
+
+            /// Returns whether the queue contains events.
+
+            bool empty() const
+            {
+                return evt_queue.empty();
+            }
+
+            /// Returns the number of events in the queue.
+
+            size_type size() const
+            {
+                return evt_queue.size();
+            }
+
+            /// Returns the front of the queue (oldest event).
+
+            const value_type& front() const
+            {
+                return evt_queue.front();
+            }
+
+            /// Returns the back of the queue (newest event).
+
+            const value_type& back() const
+            {
+                return evt_queue.back();
+            }
+
+            /// Pops the front of the queue.
+
+            bool pop()
+            {
+                if(empty())
+                {
+                    return false;
+                }
+                evt_queue.pop();
+                return true;
+            }
+
+        protected:
+
+            /// Pushes to the back of the queue.
+
+            bool fill_event(const event_type& evt)
+            {
+                evt_queue.push(evt);
+            }
+
+        private:
+
+            std::queue<event_type> evt_queue;
+    };
+}
+
+#endif /*CAMGEN_EVT_QUEUE_H_*/
+
